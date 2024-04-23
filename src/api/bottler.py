903dbdd015a0f958d1fab94193659493939d71c5
Dre Suguitan <ddre2026@gmail.com>
2024-04-22 20:13:38 -0700

    stopping bottling for now

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -112,6 +112,7 @@ def get_bottle_plan():
 
     with db.engine.begin() as connection:
         list = []
+        return list
 
         # Get the maxPotions I can make
         maxToMake = (connection.execute(sqlalchemy.text("SELECT pot_capacity FROM global_inventory")).scalar_one()
