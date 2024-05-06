e21129a7f0499730e28937e9c2055b9f41d2f164
Dre Suguitan <ddre2026@gmail.com>
2024-05-06 00:16:11 -0700

    new catalog logic (pls help me)

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,41 +15,97 @@ def get_catalog():
     list = []
     
     with db.engine.begin() as connection:
-        
-        table = connection.execute(sqlalchemy.text(
+        day = connection.execute(sqlalchemy.text(
             """
-            SELECT 
-                potions.id AS id, 
-                sku,
-                name, 
-                COALESCE(SUM(entries.quantity), 0) AS sum_quantity,
-                price, 
-                potion_type
-            FROM potion_inventory AS potions
-            LEFT JOIN potion_ledger_entries AS entries 
-            ON potions.id = entries.potion_id
-            GROUP BY potions.id
-            HAVING COALESCE(SUM(entries.quantity), 0) > 0
-            ORDER BY RANDOM()
-            LIMIT 6
-            """
+            WITH max_id AS(
+            SELECT MAX(id) AS last_id
+            FROM ticks
+            )
+            SELECT day
+            FROM max_id 
+            JOIN ticks
+            ON max_id.last_id = ticks.id
+            LIMIT 1
+            """ 
         ))
+        print(f"day: {day}")
+        
+        table = connection.execute(
+            sqlalchemy.text(
+                """
+                select 
+                subquery3.potion_id,
+                sum_purchases_on_day, 
+                sum_purchases_general,
+                rn,
+                sku,
+                name,
+                price,
+                potion_type,
+                quantity
+
+                from
+                (select *,
+                    row_number() over (
+                        order by 
+                        sum_purchases_on_day desc,
+                        sum_purchases_general desc
+                    ) as rn
+                from
+                    (select 
+                    subquery.potion_id, 
+                    coalesce(sum(case 
+                        when ticks.day = :day and quantity < 0 then quantity * -1
+                        else 0 end
+                        ), 0) as sum_purchases_on_day,
+                    coalesce(sum(case
+                        when quantity < 0 then quantity * -1
+                        else 0 end
+                    ), 0) as sum_purchases_general
+                    from 
+                    (select potion_id
+                    from potion_inventory_view
+                    where quantity > 0) as subquery
+
+                    join potion_ledger_entries as entries
+                    on subquery.potion_id = entries.potion_id
+                    left join ticks
+                    on entries.tick_id = ticks.id
+                    group by subquery.potion_id
+                    ) as subquery2
+                order by 
+                    case 
+                    when sum_purchases_general > 0 then 1
+                    else 1 + random() end,
+                    sum_purchases_on_day desc,
+                    sum_purchases_general desc
+                ) as subquery3
+                join potion_inventory_view as potions 
+                on potions.potion_id = subquery3.potion_id
+                order by 
+                case
+                    when rn <= 4 then rn
+                    else 4 + random() end
+                limit 6
+                """
+            ),
+            [{
+                "day": 'Hearthday'
+            }]
+        )
             # GROUP BY potions.id
             # ORDER BY quantity DESC
 
         for potion in table:
-            if potion.sum_quantity is None:
-                continue
-            if potion.sum_quantity > 0:
-                list.append(
-                    {
-                        "sku": potion.sku,
-                        "name": potion.name,
-                        "quantity": potion.sum_quantity,
-                        "price": potion.price,
-                        "potion_type": potion.potion_type
-                    }
-                )
+            list.append(
+                {
+                    "sku": potion.sku,
+                    "name": potion.name,
+                    "quantity": potion.quantity,
+                    "price": potion.price,
+                    "potion_type": potion.potion_type
+                }
+            )
 
     # print(list)
 
